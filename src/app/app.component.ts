52f4cd9f90f62569f3f1adfe2b1ce13792deff31
B-Yamawaki <bzyamawaki@gmail.com>
2024-03-03 01:52:59 -0800

    Created Sign In page and put button on it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, FormsModule, NavbarComponent],
+  imports: [RouterOutlet, MatSlideToggleModule, FormsModule, NavbarComponent, SignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
