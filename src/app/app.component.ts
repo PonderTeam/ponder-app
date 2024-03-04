7831631e3fde2522a14b21bd5b29b00473ef9a01
Dylan H <dylanhuynh001@gmail.com>
2024-03-03 18:46:44 -0800

    Redid html and looking at the div border

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
+import { StudyFlashcardComponent } from './study-flashcard/study-flashcard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, FormsModule, NavbarComponent],
+  imports: [RouterOutlet, MatSlideToggleModule, FormsModule, NavbarComponent, StudyFlashcardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
