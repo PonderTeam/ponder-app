6efee5f0f1300d05b7862ac52ea9d91b4739620d
Dylan Huynh <dylanhuynh001@gmail.com>
2024-03-04 18:18:02 -0800

    Created study sequence component

---
+++ src/app/study-sequence/study-sequence.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-study-sequence',
+  standalone: true,
+  imports: [],
+  templateUrl: './study-sequence.component.html',
+  styleUrl: './study-sequence.component.scss'
+})
+export class StudySequenceComponent {
+
+}
