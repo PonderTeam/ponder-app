6eb0e64c5bc20a3175a3bc9ed441061bec309662
JeremyLin003 <80878588+JeremyLin003@users.noreply.github.com>
2024-03-04 20:06:06 -0800

    Isolated Edit-Create Parent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { EditCreateStudySetComponent } from './edit-create-study-set/edit-create-study-set.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { StudyFlashcardComponent } from './study-flashcard/study-flashcard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'signIn',
+    component: SignInComponent,
+    title:'Sign In Page'
+  },
+  {
+    path: 'editCreateSet',
+    component: EditCreateStudySetComponent,
+    title:'Edit Create Study Set Page'
+  },
+  {
+    path: 'studyFlashcard',
+    component: StudyFlashcardComponent,
+    title:'Study Flashcard Page'
+  },
+
+];
