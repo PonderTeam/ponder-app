44874dd1e89d2a4469d530bafa94d7061750a343
B-Yamawaki <bzyamawaki@gmail.com>
2024-03-07 11:54:46 -0800

    Formatted the tab content divs of the page
    Turned sections into divs. Reorganized and relabeled classes. Applied relative formatting.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,11 @@ import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 
-import { ViewstudysetComponent } from './viewstudyset/viewstudyset.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule,RouterOutlet, MatSlideToggleModule, FormsModule, NavbarComponent,
-    SignInComponent,ViewstudysetComponent],
+    SignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
