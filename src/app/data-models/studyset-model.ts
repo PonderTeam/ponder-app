775b6cf6abd2e75fc10d9e1cbedd2ec565fce50f
ashkjones <ashkjones@gmail.com>
2024-03-08 12:58:25 -0800

    added additional functionality to the StudySet model classes

--- src/app/data-models/studyset-model.ts
+++ src/app/data-models/studyset-model.ts
@@ -29,6 +29,8 @@ export class StudySetData implements StudySetModel {
   description: string;
   flashcards: FlashcardModel[];
   sequences: SequenceModel[];
+  private nextFid = 0;  // next available flashcard id
+  private nextSid = 0; // next available sequence id
 
   constructor(
     owner: string = "",
@@ -43,7 +45,8 @@ export class StudySetData implements StudySetModel {
     this.description = description.trim();
     this.flashcards = flashcards;
     this.sequences = sequences;
-    this.id = id = id;
+    this.id = id;
+    this.initiateNextIds();
   }
 
   isValid(): boolean {
@@ -70,6 +73,46 @@ export class StudySetData implements StudySetModel {
   }
 
   /**
+   * Initiates the next available flashcard and sequence id.
+   */
+  private initiateNextIds(): void {
+    let high: number = 0;
+    for (const card of this.flashcards) {
+      if (!card) {
+        break;
+      } else if (high < card.id) {
+        high = card.id;
+      }
+    }
+    this.nextFid = high + 1; // set the next available id
+
+    high = 0; // reset high
+
+    for (const seq of this.sequences) {
+      if (!seq) {
+        break;
+      } else if (high < seq.id) {
+        high = seq.id;
+      }
+    }
+    this.nextSid = high + 1; // set the next available id
+  }
+
+  /**
+   * Increments the next available flashcard id.
+   */
+  private incrementNextFid() {
+    this.nextFid++; // simple wrapper incase we change this algo
+  }
+
+  /**
+   * Increments the next available sequence id.
+   */
+  private incrementNextSid() {
+    this.nextSid++; // simple wrapper incase we change this algo
+  }
+
+  /**
    * Will rebuild sequences to reference Flashcard objects in
    * flashcards collection rather using duplicated Flashcard objects.
    * Run this method when pulling after requesting data from a server that
@@ -91,6 +134,9 @@ export class StudySetData implements StudySetModel {
     )
   }
 
+  /**
+   * Makes a copy of StudySetData Object from a "study set like" object.
+   */
   static copyStudySet(oldSet: StudySetModel): StudySetData {
     let newSet = new StudySetData(
       oldSet.owner,
@@ -108,6 +154,63 @@ export class StudySetData implements StudySetModel {
     )
     newSet.rebuildSequences();
     return newSet;
+  }
+
+  /**
+   * Moves a card to new position in the flashcard list and shifts the other cards
+   * to accommodate the reorder.
+   * @param card flashcard to move
+   * @param to index to move card "to"
+   * @returns 1 if move succeeded, 0 if card not found, -1 if pos outside range.
+   */
+  reorderCard(card: FlashcardModel, to: number): number {
+    if (!(to >= 0 && to < this.flashcards.length)) {
+      return -1 // outside range
+    }
+    const from = this.flashcards.indexOf(card, 0);
+    if (from > -1) {
+      this.flashcards.splice(from, 1)
+      this.flashcards.splice(to, 0, card)
+    }
+    return 0; // card not found
+  }
+
+  /** Create and add a flashcard to the study set. */
+  addCard(term: string, definition: string, image?: URL): void {
+    this.flashcards.push(new FlashcardData(term, definition, image, this.nextFid));
+    this.incrementNextFid();
+  }
+
+  /** Removed a card from flashcard list and sequences. */
+  deleteCard(card: FlashcardModel): boolean {
+    // lookup is O(n) but deletes won't be too common
+    const index = this.flashcards.indexOf(card, 0);
+    if (index > -1) {
+      this.flashcards.splice(index, 1)
+      for (const seq of this.sequences) {
+        if (!seq) {
+          continue;
+        }
+        // force cast masks errors by returning undefined if not an instance of SequenceData
+        ((seq as unknown) as SequenceData).removeCard(card);
+      }
+      return true;
+    }
+    return false;
+  }
+
+  /** Creates a sequence in the study set */
+  addSequence(name: string, cardList: FlashcardModel[] = []): void {
+    this.sequences.push(new SequenceData(name, cardList, this.nextSid))
+    this.incrementNextSid();
+  }
+
+  /** Deletes a sequence from the study set */
+  deleteSequence(seq: SequenceData): void {
+    const index = this.sequences.indexOf(seq, 0);
+    if (index > -1) {
+      this.flashcards.splice(index, 1)
+    }
   }
 }
 
