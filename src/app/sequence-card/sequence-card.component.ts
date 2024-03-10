7d9f3c03306e504568ec15f7ea2524ad3865befe
B-Yamawaki <bzyamawaki@gmail.com>
2024-03-09 23:36:53 -0800

    Created inSequence version of card

--- src/app/sequence-card/sequence-card.component.ts
+++ src/app/sequence-card/sequence-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FlashcardComponent } from '../flashcard/flashcard.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,12 +7,13 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-sequence-card',
   standalone: true,
-  imports: [FlashcardComponent, MatButtonModule, MatIconModule],
+  imports: [CommonModule, FlashcardComponent, MatButtonModule, MatIconModule],
   templateUrl: './sequence-card.component.html',
   styleUrl: './sequence-card.component.scss'
 })
 export class SequenceCardComponent extends FlashcardComponent{
-  inSequence: boolean = false;
+  term: string  = "term";
+  inSequence: boolean = true;
 
   addToSequence(e: Event) {
     e.stopPropagation();
