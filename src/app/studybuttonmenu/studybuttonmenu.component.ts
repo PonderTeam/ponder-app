9504cd3e5132a0397a1b2675156ae6ae83dfe2c7
Samuel Lee <superbankaisam@gmail.com>
2024-03-10 13:15:34 -0700

    maticonmodule

--- src/app/studybuttonmenu/studybuttonmenu.component.ts
+++ src/app/studybuttonmenu/studybuttonmenu.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatMenu } from '@angular/material/menu';
 import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-studybuttonmenu',
   standalone: true,
-  imports: [MatButtonModule, MatMenu, MatMenuTrigger],
+  imports: [MatButtonModule, MatMenu, MatMenuTrigger, MatIconModule],
   templateUrl: './studybuttonmenu.component.html',
   styleUrl: './studybuttonmenu.component.scss'
 })
