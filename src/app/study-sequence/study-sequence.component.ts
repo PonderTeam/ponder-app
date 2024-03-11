7b86c2dfe8b1065ab335117f2be39d62e6bec801
Dylan H <dylanhuynh001@gmail.com>
2024-03-10 23:52:34 -0700

    Created basic expansion panel

--- src/app/study-sequence/study-sequence.component.ts
+++ src/app/study-sequence/study-sequence.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { SequenceCardComponent } from '../sequence-card/sequence-card.component';
 import { FlashcardComponent } from '../flashcard/flashcard.component';
 
+import {MatExpansionModule} from '@angular/material/expansion'
 @Component({
   selector: 'app-study-sequence',
   standalone: true,
-  imports: [SequenceCardComponent, FlashcardComponent],
+  imports: [SequenceCardComponent, FlashcardComponent, MatExpansionModule],
   templateUrl: './study-sequence.component.html',
   styleUrl: './study-sequence.component.scss'
 })
