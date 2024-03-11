ba5b7b24a123e3dbbd2c99aa61faefb64f37790c
Samuel Lee <superbankaisam@gmail.com>
2024-03-10 20:25:45 -0700

    removed imports of button, should be imports of view study

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,10 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SignInComponent } from './sign-in/sign-in.component';
-import { StudybuttonmenuComponent } from './studybuttonmenu/studybuttonmenu.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatSlideToggleModule, FormsModule, NavbarComponent, SignInComponent,
-    StudybuttonmenuComponent],
+  imports: [CommonModule, RouterOutlet, MatSlideToggleModule, FormsModule, NavbarComponent, SignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
