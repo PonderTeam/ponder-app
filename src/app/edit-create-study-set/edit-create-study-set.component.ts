7d760d1eb433559be75bfbadbd9da232a2cfe21d
JeremyLin003 <80878588+JeremyLin003@users.noreply.github.com>
2024-03-11 23:43:07 -0700

    Flashcard

--- src/app/edit-create-study-set/edit-create-study-set.component.ts
+++ src/app/edit-create-study-set/edit-create-study-set.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButton, MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
@@ -9,11 +9,12 @@ import { RouterLink } from '@angular/router';
 import { CustomTabsModule } from '../custom-tabs/custom-tabs.module';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { FlashcardComponent } from '../flashcard/flashcard.component';
 
 @Component({
   selector: 'app-edit-create-study-set',
   standalone: true,
-  imports: [CommonModule,MatButtonModule, MatInputModule,MatFormFieldModule,FormsModule,MatButtonToggleModule, CustomTabsModule, MatCardModule, MatIconModule, RouterLink],
+  imports: [CommonModule,MatButtonModule, MatInputModule,MatFormFieldModule,FormsModule,MatButtonToggleModule, CustomTabsModule, MatCardModule, MatIconModule, RouterLink, FlashcardComponent],
   templateUrl: './edit-create-study-set.component.html',
   styleUrl: './edit-create-study-set.component.scss'
 })
@@ -22,4 +23,10 @@ export class EditCreateStudySetComponent {
   description = '';
   term = '';
   definition = '';
+  cardScaleFactor: number = window.innerWidth * (880 / 1280) / 1700;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.cardScaleFactor = window.innerWidth * (880 / 1280) / 1700;
+  }
 }
