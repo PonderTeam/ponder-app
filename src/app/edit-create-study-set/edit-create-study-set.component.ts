0381ef01bf0feb8f4ac359884bd23064c725ac8d
JeremyLin003 <80878588+JeremyLin003@users.noreply.github.com>
2024-03-12 00:11:54 -0700

    Tab content into separate component
    I moved the content within the tabs into two new components.
    
    I am not liable for anything that breaks with this change.
    
    I tried to remove superfluous code, but definitnely didnt catch everything.

--- src/app/edit-create-study-set/edit-create-study-set.component.ts
+++ src/app/edit-create-study-set/edit-create-study-set.component.ts
@@ -1,6 +1,6 @@
 import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -10,23 +10,18 @@ import { CustomTabsModule } from '../custom-tabs/custom-tabs.module';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { FlashcardComponent } from '../flashcard/flashcard.component';
+import { FlashcardEditorComponent } from '../flashcard-editor/flashcard-editor.component';
+import { SequenceEditorComponent } from '../sequence-editor/sequence-editor.component';
 
 @Component({
   selector: 'app-edit-create-study-set',
   standalone: true,
-  imports: [CommonModule,MatButtonModule, MatInputModule,MatFormFieldModule,FormsModule,MatButtonToggleModule, CustomTabsModule, MatCardModule, MatIconModule, RouterLink, FlashcardComponent],
+  imports: [CommonModule,MatButtonModule, MatInputModule,MatFormFieldModule,FormsModule,MatButtonToggleModule, CustomTabsModule,
+            MatCardModule, MatIconModule, RouterLink, FlashcardComponent,FlashcardEditorComponent,SequenceEditorComponent],
   templateUrl: './edit-create-study-set.component.html',
   styleUrl: './edit-create-study-set.component.scss'
 })
 export class EditCreateStudySetComponent {
   title = '';
   description = '';
-  term = '';
-  definition = '';
-  cardScaleFactor: number = window.innerWidth * (880 / 1280) / 1700;
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: Event) {
-    this.cardScaleFactor = window.innerWidth * (880 / 1280) / 1700;
-  }
 }
