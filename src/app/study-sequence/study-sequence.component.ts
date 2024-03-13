c2f85309509fd11fc6a16140b3de9eebf3b9bd26
Dylan Huynh <dylanhuynh001@gmail.com>
2024-03-13 10:22:18 -0700

    Convert hardcoded data, into variables

--- src/app/study-sequence/study-sequence.component.ts
+++ src/app/study-sequence/study-sequence.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SequenceCardComponent } from '../sequence-card/sequence-card.component';
 import { FlashcardComponent } from '../flashcard/flashcard.component';
 import { ReturnRibbonComponent } from '../return-ribbon/return-ribbon.component';
@@ -7,10 +8,11 @@ import {MatExpansionModule} from '@angular/material/expansion'
 @Component({
   selector: 'app-study-sequence',
   standalone: true,
-  imports: [SequenceCardComponent, FlashcardComponent, ReturnRibbonComponent, MatExpansionModule],
+  imports: [CommonModule, SequenceCardComponent, FlashcardComponent, ReturnRibbonComponent, MatExpansionModule],
   templateUrl: './study-sequence.component.html',
   styleUrl: './study-sequence.component.scss'
 })
 export class StudySequenceComponent {
-
+  sequenceTitle: String = "Sequence Name";
+  sequenceName: String[] = ['Sequence 1', 'Sequence 2', 'Sequence 3']
 }
