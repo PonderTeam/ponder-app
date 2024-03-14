eceeb3f42eafc753ffc36730883be4002db22552
ashkjones <ashkjones@gmail.com>
2024-03-14 14:20:41 -0700

    ec flashcard drag is working

--- src/app/flashcard-editor/flashcard-editor.component.ts
+++ src/app/flashcard-editor/flashcard-editor.component.ts
@@ -9,7 +9,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { EcCardPreviewComponent } from '../ec-card-preview/ec-card-preview.component';
 import { FlashcardData } from '../data-models/flashcard-model';
-import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray} from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  CdkDragStart,
+  DragDropModule,
+  moveItemInArray
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-flashcard-editor',
@@ -24,23 +29,30 @@ import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray} from '@angular/cdk/d
     MatCardModule,
     MatIconModule,
     EcCardPreviewComponent,
-    CdkDropList,
-    CdkDrag
+    DragDropModule
   ],
   templateUrl: './flashcard-editor.component.html',
   styleUrl: './flashcard-editor.component.scss'
 })
 export class FlashcardEditorComponent {
   @Input() flashcards: FlashcardData[] = [new FlashcardData];
-  @Output() selectedCard = new EventEmitter<FlashcardData>;
-  selected: FlashcardData = this.flashcards[0];
+  @Output() selectCardEvent = new EventEmitter<FlashcardData>;
+  selectedCard: FlashcardData = this.flashcards[0];
+  highlight: boolean = true;
+
+  drag(event: CdkDragStart) {
+    this.highlight = false;
+    console.log("bloop")
+  };
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.flashcards, event.previousIndex, event.currentIndex)
+    this.highlight = true;
   }
 
   onPreviewSelect(flashcard: FlashcardData) {
-    this.selectedCard.emit(flashcard);
-    console.log("Selected", flashcard);
+    this.selectedCard = flashcard;
+    this.selectCardEvent.emit(this.selectedCard);
+    this.highlight = true;
   }
 }
