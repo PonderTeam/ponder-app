22dd2e42b6babd03c6e5bc5a77398fe1f4a01869
Dylan H <dylanhuynh001@gmail.com>
2024-03-15 01:23:08 -0700

    Made sequence select into a menu now

--- src/app/study-sequence/study-sequence.component.ts
+++ src/app/study-sequence/study-sequence.component.ts
@@ -4,11 +4,15 @@ import { SequenceCardComponent } from '../sequence-card/sequence-card.component'
 import { FlashcardComponent } from '../flashcard/flashcard.component';
 import { ReturnRibbonComponent } from '../return-ribbon/return-ribbon.component';
 
-import {MatExpansionModule} from '@angular/material/expansion'
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatMenuTrigger } from '@angular/material/menu';
+
 @Component({
   selector: 'app-study-sequence',
   standalone: true,
-  imports: [CommonModule, SequenceCardComponent, FlashcardComponent, ReturnRibbonComponent, MatExpansionModule],
+  imports: [CommonModule, SequenceCardComponent, FlashcardComponent, ReturnRibbonComponent, MatButtonModule, MatMenuModule, MatMenuTrigger, MatIconModule],
   templateUrl: './study-sequence.component.html',
   styleUrl: './study-sequence.component.scss'
 })
