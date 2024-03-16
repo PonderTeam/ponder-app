6a07a1532caa3f7c8c9af0e31cd5bd1a1ec19523
ashkjones <ashkjones@gmail.com>
2024-03-15 20:42:21 -0700

    Removed unused import and removed material theme warning

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,19 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HttpClientModule } from '@angular/common/http';
 import { StudySetDevService } from './services/study-set-dev.service';
+import { StudySetService } from './services/study-set.service';
+import { MATERIAL_SANITY_CHECKS } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     importProvidersFrom(HttpClientModule),
-    StudySetDevService
+    StudySetDevService,
+    { provide: StudySetService, useClass: StudySetDevService },
+    { provide: MATERIAL_SANITY_CHECKS, useValue: false }
   ]
 };
