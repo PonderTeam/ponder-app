b4d530070b01683081a89d752caa3c51f2f9ffc4
ashkjones <ashkjones@gmail.com>
2024-03-15 21:27:35 -0700

    Small edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { SignInComponent } from './sign-in/sign-in.component';
   standalone: true,
   imports: [CommonModule, RouterOutlet, MatSlideToggleModule, FormsModule, NavbarComponent, SignInComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Ponder';
