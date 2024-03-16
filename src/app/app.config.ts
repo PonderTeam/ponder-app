feca26d38bec463d4d5577f4e3a7cc7083b4e4a6
ashkjones <ashkjones@gmail.com>
2024-03-15 18:28:01 -0700

    Revert "Remove unused imports and provider to config file"
    This reverts commit b9d7e4c5707918f995274eb08068dcc94513e275.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,14 +5,12 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HttpClientModule } from '@angular/common/http';
 import { StudySetDevService } from './services/study-set-dev.service';
-import { StudySetService } from './services/study-set.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     importProvidersFrom(HttpClientModule),
-    StudySetDevService,
-    { provide: StudySetService, useClass: StudySetDevService }
+    StudySetDevService
   ]
 };
