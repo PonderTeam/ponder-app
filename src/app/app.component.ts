dbc89127c02e538400f7a81c95396e7b51619b9b
ashkjones <ashkjones@gmail.com>
2024-03-15 18:28:01 -0700

    Revert "Remove unused imports and provider to config file"
    This reverts commit b9d7e4c5707918f995274eb08068dcc94513e275.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,19 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { EditCreateStudySetComponent } from './edit-create-study-set/edit-create-study-set.component';
+import { StudySetService } from './services/study-set.service';
+import { StudySetDevService } from './services/study-set-dev.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatSlideToggleModule, FormsModule, NavbarComponent, SignInComponent],
+  imports: [CommonModule, RouterOutlet, MatSlideToggleModule, FormsModule, NavbarComponent, SignInComponent, EditCreateStudySetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  providers: [
+    { provide: StudySetService, useClass: StudySetDevService }
+  ]
 })
 export class AppComponent {
   title = 'Ponder';
