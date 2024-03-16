dd03f91e92ac7b57128a5a622cd609c8c5db582d
B-Yamawaki <bzyamawaki@gmail.com>
2024-03-16 04:18:24 -0700

    save set revised with new function calls

--- src/app/services/study-set-firebase.service.ts
+++ src/app/services/study-set-firebase.service.ts
@@ -1,14 +1,13 @@
 import { Injectable, Inject } from '@angular/core';
 import { StudySetService } from './study-set.service';
 import { StudySetData, StudySetModel } from '../data-models/studyset-model';
-import { Firestore, getDoc, doc, collection, addDoc, DocumentSnapshot, DocumentReference } from '@angular/fire/firestore';
+import { Firestore, doc, collection, getDoc, addDoc, setDoc, DocumentSnapshot, DocumentReference } from '@angular/fire/firestore';
 import { Observable, defer, from, map, forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudySetFirebaseService extends StudySetService{
-  //private firestore = Inject(Firestore);
   constructor(private firestore: Firestore) {
     super();
   }
@@ -23,7 +22,7 @@ export class StudySetFirebaseService extends StudySetService{
       return forkJoin(ids.map(id => this.getStudySet(id)));
   }
 
-  override saveStudySet(studySet: StudySetModel): Observable<StudySetData> {
+  override saveStudySet(studySet: StudySetModel): Observable<String> {
     if(!studySet.id){
       return defer(() => from(addDoc(collection(this.firestore, 'study-sets'), {
         owener: studySet.owner,
@@ -32,9 +31,16 @@ export class StudySetFirebaseService extends StudySetService{
         flashcards: studySet.flashcards,
         sequences: studySet.sequences
       }) as Promise<DocumentReference> ))
-      .pipe(map((docSnap: DocumentReference) => docSnap.data() as StudySetModel))
-        map((studySet) => StudySetData.copyStudySet(studySet as StudySetModel)))
+      .pipe(map((docSnap: DocumentReference) => docSnap.id))
+    } else {
+      return defer(() => from(setDoc(doc(collection(this.firestore, 'study-sets'), studySet.id), {
+        owener: studySet.owner,
+        title: studySet.title,
+        description: studySet.description,
+        flashcards: studySet.flashcards,
+        sequences: studySet.sequences
+      }) as Promise<void> ))
+      .pipe(map((ret => studySet.id as String)))
     }
-
   }
 }
