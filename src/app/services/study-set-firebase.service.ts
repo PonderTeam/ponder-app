7a2ad5ba1f28c2cf0076963e438c34186ce67cd3
B-Yamawaki <bzyamawaki@gmail.com>
2024-03-16 03:21:06 -0700

    Added getStudySets

--- src/app/services/study-set-firebase.service.ts
+++ src/app/services/study-set-firebase.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Inject } from '@angular/core';
 import { StudySetService } from './study-set.service';
 import { StudySetData, StudySetModel } from '../data-models/studyset-model';
 import { Firestore, getFirestore, doc, collection, addDoc, DocumentSnapshot } from '@angular/fire/firestore';
-import { Observable, defer, from, map } from 'rxjs';
+import { Observable, defer, from, map, forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +16,11 @@ export class StudySetFirebaseService extends StudySetService{
   override getStudySet(id: string): Observable<StudySetData> {
       return defer(() => from(this.firestore.collection('study-set').doc(id).get() as Promise<DocumentSnapshot>))
         .pipe(map((docSnap: DocumentSnapshot) => docSnap.data() as StudySetModel))
-        .pipe(map((dbSet: StudySetModel) => StudySetData.copyStudySet(dbSet)))
+        .pipe(map((dbSet: StudySetModel) => StudySetData.copyStudySet(dbSet)));
+  }
+
+  override getStudySets(ids: string[]): Observable<StudySetData[]> {
+      return forkJoin(ids.map(id => this.getStudySet(id)));
   }
 
   override saveStudySet(studySet: StudySetModel): Observable<StudySetData> {
