8cca28978ab5f3e41d2bfa1be5895fb321ded2ca
Samuel Lee <superbankaisam@gmail.com>
2024-03-16 21:41:54 -0700

    go

--- src/app/sequence-editor/sequence-editor.component.ts
+++ src/app/sequence-editor/sequence-editor.component.ts
@@ -6,6 +6,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { FlashcardComponent } from '../flashcard/flashcard.component';
 import { FlashcardEditorComponent } from '../flashcard-editor/flashcard-editor.component';
+import { StudySetService } from '../services/study-set.service';
+import { SequenceData } from '../data-models/sequence-model';
 
 @Component({
   selector: 'app-sequence-editor',
@@ -15,20 +17,32 @@ import { FlashcardEditorComponent } from '../flashcard-editor/flashcard-editor.c
   styleUrl: './sequence-editor.component.scss'
 })
 export class SequenceEditorComponent {
+  constructor(private studySetService: StudySetService) {};
   cardScaleFactor: number = window.innerWidth * (880 / 1280) / 1700;
-  data = [
-    
-  ];
+  sequences: SequenceData[] = []; //araray of empty sequnexcs
+  //isLoaded: boolean = false; // async
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
     this.cardScaleFactor = window.innerWidth * (880 / 1280) / 1700;
   }
 
-  inSequence: boolean = false; // causes flashcard to enter, and not text
+  inSequence: boolean = !false;
   addToSequence(e: Event) {
     e.stopPropagation();
-    this.inSequence = true;
+    this.inSequence = !true;
+  }
+
+  getStudySet(setId: string = "bbbb") { //hardcoded
+    this.studySetService.getStudySet(setId)
+      .subscribe(sSet => [
+        this.sequences = sSet.sequences,
+        //this.isLoaded = true,
+        console.log("before", this.sequences)
+      ]);
+  }
+
+  ngOnInit() { //temporary til Ashley PR mained
+    this.getStudySet();
   }
 }
-
