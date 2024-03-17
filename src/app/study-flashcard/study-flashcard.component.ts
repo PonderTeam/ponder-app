6071be2d13cbb6a8e3bd4faca592b9e335a27d06
Sophia Virdi <Sophia.Virdi01@student.csulb.edu>
2024-03-17 00:33:10 -0700

    Flashcard-Model.ts importing & making an array
    - I added in another import statement. This time, we are importing in the flashcard-model file from the data-models folder because we need that in addition to our services file in your Study-Flashcard component
    - I also added in a property (this is just an array) that will hold the flashcards once we bring them into our code. It's called "flashcards". It's currently just an empty array that has nothing in it.

--- src/app/study-flashcard/study-flashcard.component.ts
+++ src/app/study-flashcard/study-flashcard.component.ts
@@ -2,6 +2,7 @@ import { Component, HostListener } from '@angular/core';
 import { FlashcardComponent } from '../flashcard/flashcard.component';
 import { ReturnRibbonComponent } from '../return-ribbon/return-ribbon.component';
 import { StudySetService } from '../services/study-set.service';
+import { FlashcardData } from '../data-models/flashcard-model';
 
 @Component({
   selector: 'app-study-flashcard',
@@ -14,6 +15,13 @@ export class StudyFlashcardComponent {
   constructor(private studySetService: StudySetService) { }
 
   cardScaleFactor: number = window.innerWidth * (880 / 1280) / 500;
+  //property named 'flashcards':
+  flashcards: FlashcardData[] = []; 
+  /*
+  'flashcards' property is an array that holds objects of type FlashcardData.
+  The part after the equal sign is an empty array. 
+  We are assigning the array of type FlashCard Data to an empty array.
+  */
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
