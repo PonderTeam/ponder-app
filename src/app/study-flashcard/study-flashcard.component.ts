dee384acad7ba5a492e2d03bd788088af0d28888
Sophia Virdi <Sophia.Virdi01@student.csulb.edu>
2024-03-17 01:12:46 -0700

    Added in FetchFlashcard method
    Added in a fetchFlashcard method to get the flashcards from studySetServices

--- src/app/study-flashcard/study-flashcard.component.ts
+++ src/app/study-flashcard/study-flashcard.component.ts
@@ -3,6 +3,7 @@ import { FlashcardComponent } from '../flashcard/flashcard.component';
 import { ReturnRibbonComponent } from '../return-ribbon/return-ribbon.component';
 import { StudySetService } from '../services/study-set.service';
 import { FlashcardData } from '../data-models/flashcard-model';
+import { StudySetData } from '../data-models/studyset-model';
 
 @Component({
   selector: 'app-study-flashcard',
@@ -27,4 +28,25 @@ export class StudyFlashcardComponent {
   onResize(event: Event) {
     this.cardScaleFactor = window.innerWidth * (880 / 1280) / 500;
   }
+}
+
+fetchFlashcards() {
+  //call the getStudySets function using studySetServices which we've injected into this component.
+  /* This method returns an observable, which is like a stream of data that we can subscribe to and 
+  we want to be notified when the data is available:
+  */
+  this.studySetService.getStudySets().subscribe(
+    //IF the data IS available, THEN we call this studySets function defined below.
+    /*This method takes a parameter that is an array of StudySetData objects which has been fetched
+    by the method call above.
+    The => arrow is just used to define the function.
+    */ 
+    (studySets: StudySetData[]) => {
+      //Now, inside the function we process the fetched data
+      //We use a flatMap function to iterate over our fetched data and extract our list of cards
+      const allFlashcards: FlashcardData[]= studySets.flatMap(set => set.flashcards);
+
+      this.flashcards = allFlashcards;
+    }
+  );
 }
