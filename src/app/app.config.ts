81587faaa9f798cbec90686bdbd92875aef4e93d
Dylan Huynh <dylanhuynh001@gmail.com>
2024-03-18 10:45:59 -0700

    Attempt at using services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,16 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HttpClientModule } from '@angular/common/http';
 import { StudySetDevService } from './services/study-set-dev.service';
+import { StudySetService } from './services/study-set.service';
+import { MATERIAL_SANITY_CHECKS } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     importProvidersFrom(HttpClientModule),
-    StudySetDevService
+    StudySetDevService,
+    {provide: StudySetService, useClass: StudySetDevService},
+    {provide: MATERIAL_SANITY_CHECKS, useValue: false}
   ]
 };
--- src/app/study-sequence/study-sequence.component.ts
+++ src/app/study-sequence/study-sequence.component.ts
@@ -3,24 +3,52 @@ import { CommonModule } from '@angular/common';
 import { SequenceCardComponent } from '../sequence-card/sequence-card.component';
 import { FlashcardComponent } from '../flashcard/flashcard.component';
 import { ReturnRibbonComponent } from '../return-ribbon/return-ribbon.component';
-
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatMenuTrigger } from '@angular/material/menu';
 
+import { StudySetService } from '../services/study-set.service';
+import { StudySetData } from '../data-models/studyset-model';
+import { SequenceData, SequenceModel } from '../data-models/sequence-model';
+
 @Component({
   selector: 'app-study-sequence',
   standalone: true,
-  imports: [CommonModule, SequenceCardComponent, FlashcardComponent, ReturnRibbonComponent, MatButtonModule, MatMenuModule, MatMenuTrigger, MatIconModule],
+  imports: [CommonModule, SequenceCardComponent, FlashcardComponent, ReturnRibbonComponent, MatButtonModule, MatMenuModule, MatMenuTrigger, MatIconModule, StudySetData, StudySetService],
   templateUrl: './study-sequence.component.html',
   styleUrl: './study-sequence.component.scss'
 })
 export class StudySequenceComponent {
-  sequenceTitle: string = "Sequence 1";
-  sequenceName: string[] = ['Sequence 1', 'Sequence 2', 'Sequence 3']
+  constructor(
+    private studySetService: StudySetService
 
-  changeName(newTitle: string){
-    this.sequenceTitle = newTitle;
+  ) {}
+
+  sequences?: SequenceData[];
+  
+  selectedSequence?: SequenceData
+  //selectedSequence: SequenceModel = this.selectedSequence.name;
+  //sequenceName: string[] = ['Sequence 1', 'Sequence 2', 'Sequence 3']
+
+  // changeSelectedSequence(newTitle: string){
+  //   this.sequenceTitle = newTitle;
+  // }
+
+  getSequences(setId: string){
+    this.studySetService.getStudySet(setId)
+    .subscribe(sSet => [
+      this.sequences = sSet.sequences,
+      this.selectedSequence = this.sequences[0],
+      // this.isLoaded = true
+    ])
+  }
+
+  changeSelectedSequence(sequence: SequenceData){
+    this.selectedSequence = sequence;
+  }
+
+  ngOnInit(){
+    this.getSequences("bbbb");
   }
 }
