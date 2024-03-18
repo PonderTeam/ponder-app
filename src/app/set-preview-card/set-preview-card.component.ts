c41ddfa6fd1f0932b81c44f0bf6f457ab2a7a90a
JeremyLin003 <80878588+JeremyLin003@users.noreply.github.com>
2024-03-18 10:23:19 -0700

    Clean up?

--- src/app/set-preview-card/set-preview-card.component.ts
+++ src/app/set-preview-card/set-preview-card.component.ts
@@ -6,7 +6,6 @@ import { CommonModule } from '@angular/common';
 import { StudySetService } from '../services/study-set.service';
 import { StudySetData } from '../data-models/studyset-model';
 import { AccessData } from '../data-models/user-model';
-import { FlashcardData, FlashcardModel } from '../data-models/flashcard-model';
 
 @Component({
   selector: 'app-set-preview-card',
@@ -19,7 +18,6 @@ export class SetPreviewCardComponent {
   @Input() setId: string | undefined;
   @Input() userId: string = "there wasn't a userId passed to this"; // remove later
   studySet: StudySetData = new StudySetData(this.userId);
-  isLoaded: boolean = false;
 
   @Input() AccessDataIn!: AccessData;
 
@@ -27,20 +25,13 @@ export class SetPreviewCardComponent {
 
   ngOnInit() {
     this.setId = this.AccessDataIn.setId;
-    if (this.setId) {
-      this.getStudySet(this.setId);
-    }
-    else {
-      this.studySet.addCard();
-      this.isLoaded = true;
-    }
+    this.getStudySet(this.setId);
   }
 
   getStudySet(setId: string) {
     this.studySetService.getStudySet(setId)
       .subscribe(sSet => [
         this.studySet = sSet,
-        this.isLoaded = true
       ]);
   }
 
