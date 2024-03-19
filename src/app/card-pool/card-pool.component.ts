d1f3dc8de7cad278d4b6b416dff8047f0d4ef555
ashkjones <ashkjones@gmail.com>
2024-03-18 19:23:49 -0700

    Change card pool data structures and passing

--- src/app/card-pool/card-pool.component.ts
+++ src/app/card-pool/card-pool.component.ts
@@ -4,6 +4,13 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { SequenceCardComponent } from '../sequence-card/sequence-card.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
+interface PoolItem {
+  card: FlashcardData,
+  show: boolean,
+  x: number,
+  y: number,
+}
+
 @Component({
   selector: 'app-card-pool',
   standalone: true,
@@ -12,14 +19,15 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
   styleUrl: './card-pool.component.scss'
 })
 export class CardPoolComponent {
-  flashcardsMap?: Map<FlashcardData, boolean> = new Map<FlashcardData, boolean>([
-    [new FlashcardData("1"), true],
-    [new FlashcardData("2"), true],
-    [new FlashcardData("3"), true],
+  flashcardsMap?: Map<string, PoolItem> = new Map<string, PoolItem>([
+    ["1", {card: new FlashcardData("1"), show:false, x:0, y:0}],
+    ["2", {card: new FlashcardData("2"), show:false, x:0, y:0}],
+    ["3", {card: new FlashcardData("1"), show:false, x:0, y:0}],
   ]);
   @Output() addToSeqEvent: EventEmitter<FlashcardData> = new EventEmitter();
-  addToSeq(flashcard: FlashcardData, show: boolean) {
-    this.flashcardsMap?.set(flashcard, show);
-    this.addToSeqEvent.emit(flashcard);
+
+  addToSeq(item: PoolItem) {
+    item.show = false;
+    this.addToSeqEvent.emit(item.card);
   }
 }
