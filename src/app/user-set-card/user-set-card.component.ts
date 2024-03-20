d30fe7059f067aec2702f75408bd4474c8b6e0ae
JeremyLin003 <80878588+JeremyLin003@users.noreply.github.com>
2024-03-20 00:04:00 -0700

    Cleaning up code

--- src/app/user-set-card/user-set-card.component.ts
+++ src/app/user-set-card/user-set-card.component.ts
@@ -18,37 +18,26 @@ export class UserSetCardComponent {
   @Input() setId: string | undefined;
   @Input() userId: string = "there wasn't a userId passed to this"; // remove later
   studySet: StudySetData = new StudySetData(this.userId);
-  isLoaded: boolean = false;
 
   @Input() AccessDataIn!: AccessData;
-  viewDate: Date|number|string |undefined;
-
+  viewDate: string = "no date";
 
   constructor(private studySetService: StudySetService) {};
 
   ngOnInit() {
     this.setId = this.AccessDataIn.setId;
-    if (this.setId) {
-      this.getStudySet(this.setId);
-    }
-    else {
-      this.studySet.addCard();
-      this.isLoaded = true;
-    }
+    this.getStudySet(this.setId);
     this.viewDate = this.AccessDataIn.viewed.toLocaleDateString(undefined, {
         day: 'numeric',
         month: 'numeric',
         year: '2-digit'
     });
-
-
   }
 
   getStudySet(setId: string) {
     this.studySetService.getStudySet(setId)
       .subscribe(sSet => [
         this.studySet = sSet,
-        this.isLoaded = true
       ]);
   }
 }
