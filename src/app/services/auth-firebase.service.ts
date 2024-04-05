4afefd242d5077e2f10c106449fa7192e248551c
euilee0129 <Samuel.Lee04@student.csulb.edu>
2024-04-05 14:47:00 -0700

    auth-firebase-service.ts
    implemented first cycle of signin capability
    signInWithGoogle()
    session, updating localStorage
    signOut()
    removing item from localStorage,
    ending session
    
    test after Jeremy part

--- src/app/services/auth-firebase.service.ts
+++ src/app/services/auth-firebase.service.ts
@@ -1,23 +1,31 @@
 import { Injectable } from '@angular/core';
 import { GoogleAuthProvider, getAuth, Auth, signInWithPopup } from '@angular/fire/auth';
-import { Observable, defer, from, map } from 'rxjs';
+import { Observable, defer, from } from 'rxjs';
 import { AuthService } from './auth.service';
+import { UserInfoService } from '../services/user-info.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthFirebaseService extends AuthService{
-  constructor(private auth: Auth = getAuth()) {
+  constructor(private auth: Auth = getAuth(), private userInfoService: UserInfoService) {
     super();
   }
 
   override signInWithGoogle(): Observable<string>{
-    return defer(() => from(signInWithPopup(this.auth, new GoogleAuthProvider))).pipe(
-      map((result) =>
-      result.user?.uid as string))
+    
+    return defer(() => from(signInWithPopup(this.auth, new GoogleAuthProvider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      localStorage.setItem('token', JSON.stringify(credential?.accessToken));
+      this.userInfoService.loadUser(result.user.uid)
+      return result.user.uid;
+    })));
   }
 
   override signOut(): void{
     this.auth.signOut();
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
   }
 }
