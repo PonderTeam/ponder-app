c88b03ff12b82afe93ad5c44863d93009429d364
B-Yamawaki <bzyamawaki@gmail.com>
2024-04-07 01:01:19 -0700

    Reverted change to flashcard initialization

--- src/app/flashcard/flashcard.component.ts
+++ src/app/flashcard/flashcard.component.ts
@@ -1,5 +1,5 @@
 // flashcard.component.ts
-import { Component, ContentChild, Input, TemplateRef, HostListener, OnInit } from '@angular/core';
+import { Component, Input, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { FlashcardData } from '../data-models/flashcard-model';
@@ -13,33 +13,24 @@ import { FlashcardData } from '../data-models/flashcard-model';
 })
 
 export class FlashcardComponent implements OnInit{
-  // Used to get additional template + styling from other components
-  @ContentChild(TemplateRef) addOns!: TemplateRef<any>;
-  // Gets a scale factor to resize the component
   @Input() scaleFactor: number = 1;
-  // Input property so parent component can pass FlashcardData object to the component 
-  @Input() flashcard: FlashcardData | undefined;
+  @Input() flashcard: FlashcardData = new FlashcardData('error', 'error');
 
-  //calculates initial size of the flashcard
   height: number = 282 * this.scaleFactor;
   width: number = 500 * this.scaleFactor;
 
-  //isFlipped is intially set to false to indicate that card is not flipped by default
   isFlipped: boolean = false;
 
-  //initial height and width of flashcard is set based on the scaleFactor
   ngOnInit(): void {
     this.onResize();
   }
 
-  //Listens for window size change and adjusts height and width of flashcard as needed
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.height = 282 * this.scaleFactor;
     this.width = 500 * this.scaleFactor;
   }
 
-  //method shows front or back of card. Upon method call, property is set to true to flip to other side. 
   flipCard() {
     this.isFlipped = !this.isFlipped;
   }
