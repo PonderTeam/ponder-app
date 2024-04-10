ec989fc97c19d1080b3f9af08131678d067eb7c2
ashkjones <ashkjones@gmail.com>
2024-04-10 08:39:14 -0700

    top search bar component functioning but not styled

---
+++ src/app/top-search-bar/top-search-bar.component.ts
@@ -0,0 +1,72 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import {AsyncPipe} from '@angular/common';
+import {MatAutocompleteModule, MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { QuerySuggestionService } from '../services/query-suggestions/query-suggestion.service';
+
+@Component({
+  selector: 'app-top-search-bar',
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe
+  ],
+  templateUrl: './top-search-bar.component.html',
+  styleUrl: './top-search-bar.component.scss',
+})
+export class TopSearchBarComponent implements OnInit {
+  formControl = new FormControl('');
+  options: string[] = [];
+  filteredOptions?: Observable<string[]>;
+  selected: string = '';
+
+  constructor(private suggestionService: QuerySuggestionService) {}
+
+  ngOnInit(): void {
+      this.filteredOptions = this.formControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        const name = typeof value === 'string' ? value : value;
+        return name ? this._filter(name as string) : this.options.slice();
+      }),
+    );
+  }
+
+  displayFn(option: string): string {
+    return option;
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  updateSuggestions(value: string) {
+    // might want to optimize to reduce pings to typesense if time permits
+    this.suggestionService.getSearchSuggestions(value)
+      .then(searches => this.options = searches);
+  }
+
+  onResultSelection(event: MatAutocompleteSelectedEvent) {
+    this.getSearchResults(event.option.value);
+  }
+
+  onKeyboardEnter(value: string, e: Event) {
+    e.stopPropagation();
+    this.getSearchResults(value);
+  }
+
+  getSearchResults(query: string) {
+    if (this.selected != query) {
+      this.selected = query;
+    }
+  }
+}
