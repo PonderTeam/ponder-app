77287220230b3cfbda96a1a8bd5b92afebe860be
ashkjones <ashkjones@gmail.com>
2024-04-11 18:22:32 -0700

    add back enviroment files and add scoped search key
    The key only has access to two collections and hopefully gitguard doesn't yell at me again... the internet says adding the seach key is safe but it yelled at me last time

---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  useFirebase: true,
+  useTypesense: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAe-LRL9nl_QioUv8HnPjF-gr7X-ccurcY",
+    authDomain: "ponder-hosting.firebaseapp.com",
+    projectId: "ponder-hosting",
+    storageBucket: "ponder-hosting.appspot.com",
+    messagingSenderId: "153886428628",
+    appId: "1:153886428628:web:338f9695d9d07707af9a41",
+    measurementId: "G-5SST355B0D"
+  },
+  typesenseConfig: {
+    host: "eufokwas6jrghymzp-1.a1.typesense.net",
+    port: 443,
+    protocol: "https",
+    searchApiKey: "dCFeImcoTFKxMh1TelD44Bc4dYePKqPU"
+  }
+};
