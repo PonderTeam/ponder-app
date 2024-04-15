0e741d45ea5c0ad7b4ae57e9fdb31ee3edad8a55
B-Yamawaki <bzyamawaki@gmail.com>
2024-04-15 10:55:04 -0700

    reorganized services into modules

---
+++ src/app/services/study-set/study-set-module.ts
@@ -0,0 +1,7 @@
+/**
+ * @module
+ * @description Imports all Services for authentication
+ */
+export * from './study-set.service';
+export * from './study-set-dev.service';
+export *  from './study-set-firebase.service';
