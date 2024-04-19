5a76b52454d727b533d0aadd4218efa7b0a47c46
B-Yamawaki <bzyamawaki@gmail.com>
2024-04-18 23:36:33 -0700

    loadImage implemented

--- src/app/services/image/image-firebase.service.ts
+++ src/app/services/image/image-firebase.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject, Inject } from '@angular/core';
 import { ImageService } from './image.service';
 import { Observable, defer, from, map } from 'rxjs';
-import { FirebaseStorage, getStorage, ref, uploadBytes, uploadString } from '@angular/fire/storage';
+import { FirebaseStorage, getStorage, ref, uploadBytes } from '@angular/fire/storage';
 import { FirebaseApp } from '@angular/fire/app';
 import { v4 as uuid } from 'uuid';
 
@@ -16,16 +16,13 @@ export class ImageFirebaseService extends ImageService{
    }
 
   override loadImage(path: string): string {
-    if(path.substring(0, 15) == "assets/images/"){
-      return path;
-    }
-    return "assets/images/lemon-pic.jpeg";
+    return "https://firebasestorage.googleapis.com/v0/b/ponder-hosting.appspot.com/o/images%2F" + path + "?alt=media";
   }
 
   override uploadImage(image: File): Observable<string> {
     const imageId = 'images/' + uuid();
     const imageRef = ref(this.storage, imageId);
-    return defer(() => from(uploadBytes(imageRef, image))).pipe(map(() => {
+    return defer(() => from(uploadBytes(imageRef, image, {cacheControl: 'public, max-age=86400' }))).pipe(map(() => {
       return imageRef.name
     }))
   }
